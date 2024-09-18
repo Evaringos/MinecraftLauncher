556ba60100bf729c19aca105042541c2fc1c4d1d
Xeenomiya <Ketov1109@gmail.com>
2024-09-18 23:40:22 +0300

    Update Themes.py

--- Themes.py
+++ Themes.py
@@ -12,6 +12,10 @@ from PyQt5 import QtWidgets
 class ThemeNew(QtWidgets.QMainWindow):
     def __init__(self):
         super(ThemeNew,self).self.__init__()
+        self.timer = QtCore.QTimer(self)
+        self.timer.timeout.connect(self.update_particles)
+        self.timer.start(50)  # Set the timer to fire every 50ms
+        
     def SetTheme(self, theme):
         if theme == "Classic92": ThemeNew.Classic92(self)
         elif theme == "AoHClassic": ThemeNew.AoHClassic(self)
@@ -26,6 +30,9 @@ class ThemeNew(QtWidgets.QMainWindow):
             background-color: #330d52;
             border: 1px solid #7d1dcc;                           
             color: #b17dff;
+        }
+        QMenu:hover { /*Dropdown menu*/              
+            color: #601fc2;
         }
         QToolBar {
             border: none;
